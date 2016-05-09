15cceeef868fd44a81a227a5dd80ec37139b465b
Magnitovich <newbrain31@gmail.com>
2016-05-09 13:09:38 +0300

    development

--- src/main/java/com/example/controller/buy/SuccessfulBuyWhiskey.java
+++ src/main/java/com/example/controller/buy/SuccessfulBuyWhiskey.java
@@ -1,5 +1,9 @@
 package com.example.controller.buy;
 
+import com.example.dao.WhiskyRepository;
+import com.example.model.WhiskeyModel;
+import com.example.service.BuyWhiskyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,13 +14,23 @@ import sun.misc.Contended;
 @Controller
 public class SuccessfulBuyWhiskey {
 
+    @Autowired
+    private WhiskyRepository whiskyRepository;
+
+    @Autowired
+    private BuyWhiskyService whiskyService;
+
     @RequestMapping(value = "buySuccessfulWhisky", method = {RequestMethod.GET, RequestMethod.POST})
     public ModelAndView view(@RequestParam("nameWhiskey")String name,
                              @RequestParam("numberOrderWhisky")Integer quantityOrder) {
 
-        System.out.println("name Whisky "+name+" Quantity Order "+quantityOrder);
+        whiskyService.changeInfoInDB(name, quantityOrder);
+
+        Iterable<WhiskeyModel> all = whiskyRepository.findAll();
+
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("buy/successful");
+        modelAndView.addObject("viewAvailableWhisky", all);
+        modelAndView.setViewName("whisky");
         return modelAndView;
 
     }
--- src/main/java/com/example/service/BuyWhiskyService.java
+++ src/main/java/com/example/service/BuyWhiskyService.java
@@ -1,7 +1,36 @@
 package com.example.service;
 
-/**
- * Created by User on 09.05.2016.
- */
+import com.example.dao.WhiskyRepository;
+import com.example.model.WhiskeyModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class BuyWhiskyService {
+
+    @Autowired
+    private WhiskyRepository repository;
+
+    @Transactional
+    public void changeInfoInDB(final String name, final Integer quantityFromUI) {
+
+        List<WhiskeyModel> list = repository.findByNameWhisky(name);
+        WhiskeyModel model = list.get(0);
+//        WhiskeyModel model = list.get(0);
+
+        int quantityInDB = model.getQuantityWhisky();
+        if(quantityInDB==0) {
+            System.out.println("In DB ZERO");
+        } else {
+
+            int result = quantityInDB - quantityFromUI;
+            model.setQuantityWhisky(result);
+            repository.save(model);
+
+        }
+    }
+
 }
